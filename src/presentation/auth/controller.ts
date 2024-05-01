3ee7115b30c083f3cd323a4c9be79f9f32f2aae3
David Vazquez <dave_u@outlook.com>
2024-04-30 23:05:27 -0600

    pequeño cambio

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+
+
+export class AuthController {
+    constructor() { }
+
+    registerUser = (req: Request, res: Response) => {
+
+        res.json("RegisterUSer")
+
+    }
+
+    loginUser = (req: Request, res: Response) => {
+
+        res.json("loginUser")
+
+    }
+
+    validateEmail = (req: Request, res: Response) => {
+
+        res.json("validateEmail...")
+
+    }
+}
+
+
